e5c784d32463e0bacff22b0eb172470f9c73a718
lindamaduka <135467647+lindamaduka@users.noreply.github.com>
2024-03-28 11:24:09 +0100

    API integration

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.main.temp;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "7508ec314e6ccfad6700f5d89600ffed";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Enugu");
